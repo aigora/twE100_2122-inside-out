9ca518c65e991f97da7a118736545e868bd863d1
Glutenfreee <101390716+Glutenfreee@users.noreply.github.com>
2022-06-12 14:12:35 +0200

    Update Laberinto_Mac.c

--- Laberinto_Mac.c
+++ Laberinto_Mac.c
@@ -48,26 +48,11 @@ void crear_laberinto (char matriz[F][C], char matrizaux[F][C])
     int i, j, c, f, dist, g;
     char tipo;
 
-    //borde del laberinto
     for ( i = 0; i < F; i++)
     {
         matriz[i][0]=' ';
         matriz[i][C-1]=' ';
     }
-
-    /*for ( i = 0; i < C; i++)
-    {
-        matriz[0][i]='_';
-        matriz[F-1][i]='-';
-    }*/
-    //entrada y salida del laberinto
-
-    /*matriz[0][0]=' ';
-    matriz[0][1]=' ';
-    matriz[0][2]=' ';
-    matriz[F-1][C-1]=' ';
-    matriz[F-1][C-2]=' ';
-    matriz[F-1][C-3]=' ';*/
     
     //interior del laberinto
     for ( i = 0; i < F; i++)
